8e739cbc93c225fea39f4bd26c116454cac6e307
usong2 <dbthd6@naver.com>
2020-03-12 23:45:22 +0900

    password handling

--- user/views.py
+++ user/views.py
@@ -1,4 +1,6 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.contrib.auth.hashers import make_password
 from .models import User
 
 # Create your views here.
@@ -7,15 +9,22 @@ def register(request):
     if request.method == 'GET':
     	return render(request, 'register.html')
     elif request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
-        re_password = request.POST['re-password']
+        username = request.POST.get('username', None)
+        password = request.POST.get('password', None)
+        re_password = request.POST.get('re-password', None)
         
-        user = User(
-        	username=username,
-            password=password
-        )
-        
-        user.save()
-        
-        return render(request, 'register.html')
+        res_data = {}
+
+        if not (username and password and re_password):
+            res_data['error'] = '모든 값을 입력해주세요.'
+        elif password != re_password:
+            res_data['error'] = '비밀번호가 다릅니다!'
+        else:
+            user = User(
+                username=username,
+                password=make_password(password)
+            )
+            
+            user.save()
+            
+        return render(request, 'register.html', res_data)
