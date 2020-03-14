1f6fddd2332496fd38f485bfc992d53ffad33426
usong2 <dbthd6@naver.com>
2020-03-15 00:00:15 +0900

    login session

--- user/views.py
+++ user/views.py
@@ -1,9 +1,38 @@
 from django.http import HttpResponse
-from django.shortcuts import render
-from django.contrib.auth.hashers import make_password
+from django.shortcuts import render, redirect
+from django.contrib.auth.hashers import make_password, check_password
 from .models import User
 
 # Create your views here.
+
+def home(request):
+    user_id = request.session.get('user')
+
+    if user_id:
+        user = User.objects.get(pk=user_id)
+        return HttpResponse(user.username)
+
+    return HttpResponse('Home!')
+
+def login(request):
+    if request.method == 'GET':
+        return render(request, 'login.html')
+    elif request.method == 'POST':
+        username = request.POST.get('username', None)
+        password = request.POST.get('password', None)
+
+        res_data = {}
+        if not (username and password):
+            res_data['error'] = '모든 값을 입력해주세요.'
+        else:
+            user = User.objects.get(username=username)
+            if check_password(password, user.password):
+                request.session['user'] = user.id
+                return redirect('/')
+            else:
+                res_data['error'] = '비밀번호가 틀렸습니다.'
+                
+        return render(request, 'login.html', res_data)
 
 def register(request):
     if request.method == 'GET':
