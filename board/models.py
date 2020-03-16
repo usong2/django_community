a383bc369a977fb981a8abf68537a289b6c1be0d
usong2 <dbthd6@naver.com>
2020-03-16 21:09:57 +0900

    board list

--- board/models.py
+++ board/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Board(models.Model):
+    title = models.CharField(max_length=128, verbose_name='제목')
+    content = models.TextField(verbose_name='내용')
+    writer = models.ForeignKey('user.User', on_delete=models.CASCADE, verbose_name='비밀번호')
+    registered_dttm = models.DateTimeField(auto_now_add=True, verbose_name='등록시간')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        db_table = 'usong_board'
+        verbose_name = '유송커뮤니티 게시판'
+        verbose_name_plural = '유송커뮤니티 게시판'
--- board/views.py
+++ board/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Board
 
 # Create your views here.
+
+def board_list(request):
+    boards = Board.objects.all().order_by('-id')
+
+    return render(request, 'board_list.html', {'boards': boards})
